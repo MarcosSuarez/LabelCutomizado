083295ea4161c091824e8d59d9f9a0dc9504e250
Marcos <marcos.sua.aya@gmail.com>
2019-06-16 10:00:57 +0200

    Se agregó un texto y se modificó su presentación

--- LabelCutomizado/ViewController.swift
+++ LabelCutomizado/ViewController.swift
@@ -10,11 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelCustom: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        cambiar(label: labelCustom)
     }
 
+    
 
+    func cambiar(label: UILabel) {
+        
+        let attributeString = NSMutableAttributedString(string: label.text ?? "")
+        
+        let myShadow = NSShadow()
+        myShadow.shadowBlurRadius = 3
+        myShadow.shadowOffset = CGSize(width: 3, height: 3)
+        myShadow.shadowColor = UIColor.black
+        
+        attributeString.addAttributes([
+            NSAttributedString.Key.strokeWidth : 4,
+            NSAttributedString.Key.strokeColor : UIColor.white,
+            NSAttributedString.Key.strikethroughStyle : 1,
+            NSAttributedString.Key.strikethroughColor : UIColor.red,
+            NSAttributedString.Key.shadow: myShadow
+            ],range: NSRange(location: 0, length: attributeString.length))
+        
+        label.attributedText = attributeString
+    }
 }
 
